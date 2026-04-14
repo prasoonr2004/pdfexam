5cc1fffb626204c8552b4a2284dc71845416460b
Prasoon Rahangdale <prasoone2004@gmail.com>
2026-04-15 01:35:44 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -34,3 +34,9 @@ app.post("/chat", async (req, res) => {
 app.listen(3001, () => {
   console.log("Server running on http://localhost:3001");
 });
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
